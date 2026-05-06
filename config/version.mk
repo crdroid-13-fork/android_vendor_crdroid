dc0a0d036982a80b3b0ae94c9f9dd3856416c1e7
muralivijay <muralidharan9845@gmail.com>
2026-05-07 01:50:13 +0530

    crdroid: Bump to version 9.36
    * shake gesture, trickystore, hide applist
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 13
 PRODUCT_VERSION_MINOR = 0
 
 # Increase CR Version with each major release.
-CR_VERSION := 9.35
+CR_VERSION := 9.36
 
 # Internal version
 ifeq ($(WITH_GMS),true)
